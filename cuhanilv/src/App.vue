34404a98eeb26bbeaf11f9721e4c07da0c4f3d01
21DP2JFern <12djfernats@rvt.lv>
2023-09-28 22:23:29 +0300

    arghhhh

--- cuhanilv/src/App.vue
+++ cuhanilv/src/App.vue
@@ -6,57 +6,11 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
 
 
-    <div class="wrapper">
-      
-      <nav>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-
 
   <RouterView />
 </template>
 
 <style scoped>
 
-nav {
-  width: 200px;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 0;
-  position:absolute;
-  top:0px;
-  left:0px;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 20px;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
