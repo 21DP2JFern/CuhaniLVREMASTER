1a140682b4d2bc044ef7ca0c1136137580879586
21DP2JFern <100911261+21DP2JFern@users.noreply.github.com>
2023-10-04 13:18:23 +0000

    commit suicide

--- cuhanilv/src/components/Header.vue
+++ cuhanilv/src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="Header"></div>
+    <div id="Header">
+        <button id="menu-but"><img src="../assets/menu.png" id="menu-img"></button>
+    </div>
 </template>
 
 <style>
@@ -11,6 +13,20 @@
     height: 5vh;
     background-color: var(--color-element);
  }
+
+ #menu-but{
+    height: 5vh;
+    width: 3vw;
+    border: none;
+    padding:0;
+    background: transparent;
+ }
+
+ #menu-img{
+    height:5vh;
+    width:3vw;
+ }
+
 </style>
 
 <script>
